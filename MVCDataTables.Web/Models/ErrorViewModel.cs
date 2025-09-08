a238f10b656793c6d68b0c6b2a6ba88deba0578e
lucianogaregnani <luciano.garegnani@estudiantes.unahur.edu.ar>
2025-09-08 19:16:48 -0300

    Agregar archivos de proyecto.

---
+++ MVCDataTables.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVCDataTables.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

3452bfba7ef752bf9188163f7b2a34850e4568d3
lucianogaregnani <luciano.garegnani@estudiantes.unahur.edu.ar>
2025-09-08 21:23:50 -0300

    student controller

---
+++ MVCDataTables.Web/Controllers/StudentController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCDataTables.Infrastructure.Context;
+
+namespace MVCDataTables.Web.Controllers
+{
+    public class StudentController : Controller
+    {
+        private readonly AppDBContext _db;
+
+        public StudentController(AppDBContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
